ee918e4be6582d3d37beaf6d562109cb0da9d76b
Ivan Shvets <shvets.ivan@gmail.com>
2022-11-24 12:56:40 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,39 +2,49 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
   Grid,
   theme,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  Center,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Center>
+        <Box textAlign="center" fontSize="xl" w="30%">
+          <Grid minH="100vh" p={3}>
+            <ColorModeSwitcher justifySelf="flex-end" />
+            <StatGroup>
+              <Stat>
+                <StatLabel>Sent</StatLabel>
+                <StatNumber>345,670</StatNumber>
+                <StatHelpText>
+                  <StatArrow type="increase" />
+                  23.36%
+                </StatHelpText>
+              </Stat>
+            </StatGroup>
+            <FormControl>
+              <FormLabel>Enter ticker</FormLabel>
+              <Input type="email" />
+              <FormHelperText>
+                For example use AAPL, TSLA or AMZN
+              </FormHelperText>
+            </FormControl>
+          </Grid>
+        </Box>
+      </Center>
     </ChakraProvider>
   );
 }

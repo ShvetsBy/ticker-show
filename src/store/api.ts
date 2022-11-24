668cb6625ae081876d2deecd983ca3d0ea4e136c
Ivan Shvets <shvets.ivan@gmail.com>
2022-11-24 21:17:43 +0300

    add docker compose

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,4 +1,4 @@
-const BASE_API="http://localhost:8080"
+const BASE_API="http://0.0.0.0:8080"
 export const fetchTicker = (ticker: string) => {
     return fetch(`${BASE_API}/ticker?${new URLSearchParams({ticker})}`).then(r => r.json())
 }
